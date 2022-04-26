a803fc84e5decd7f63569fa9ef0968a8510f764f
pocu46 <andrew.pulyaev@gmail.com>
2022-04-26 19:44:08 +0300

    header component refactor + styles folder added

--- unistex/src/header/header.js
+++ unistex/src/header/header.js
@@ -2,33 +2,33 @@ import React from "react";
 import './header.scss';
 
 const Header = () => {
-    return(
-        <header className="header">
-            <div className="container top">
-                <img className="logo" src="full_logo.png" alt="icon" />
-                    <ul className="buttons">
-                        <li className="buttons-option">
-                            <a href="" className="buttons-item left">
-                                login
-                            </a>
-                        </li>
-                        <li>
-                            <a href="" className="buttons-item green">
-                                registration
-                            </a>
-                        </li>
-                        <li>
-                            <div className="language">
-                                <a href="" className="buttons-item">
-                                    en
-                                </a>
-                                <img src="arrow.png" alt="icon" />
-                            </div>
-                        </li>
-                    </ul>
+  return (
+    <header className="header">
+      <div className="container top">
+        <img className="logo" src="full_logo.png" alt="icon"/>
+        <ul className="buttons-list">
+          <li className="buttons-option">
+            <a href="" className="buttons-item left">
+              login
+            </a>
+          </li>
+          <li className="buttons-option">
+            <a href="" className="buttons-item green">
+              registration
+            </a>
+          </li>
+          <li className="buttons-option">
+            <div className="language">
+              <a href="" className="buttons-item">
+                en
+              </a>
+              <img className="arrow" src="arrow.png" alt="icon"/>
             </div>
-        </header>
-    )
+          </li>
+        </ul>
+      </div>
+    </header>
+  )
 }
 
 export default Header;
